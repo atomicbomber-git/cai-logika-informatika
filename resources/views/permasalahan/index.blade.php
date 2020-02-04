c0f8bfbb926c8b7d77d79bf084ac6fbdf1661ae4
Antonius Yonathan <atomicbomber.email@gmail.com>
2020-02-04 13:02:34 +0700

    Created project.

---
+++ resources/views/permasalahan/index.blade.php
@@ -0,0 +1,17 @@
+@extends("layouts.app")
+
+@section("content")
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                Katalog Permasalahan
+            </div>
+
+            <div class="card-body">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci asperiores culpa ea eaque excepturi exercitationem facere facilis labore laboriosam laudantium libero modi nobis quia, quidem soluta tempore! Corporis, ex.
+                </p>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use App\Http\Controllers\PermasalahanController;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::resource("permasalahan", 'PermasalahanController');
+
+Route::get('/home', 'HomeController@index')->name('home');
