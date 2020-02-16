00b40f95ab1977b31b62d47eba499883421e46a6
James Patrick Keegan <atomicbomber.email@gmail.com>
2020-02-16 12:24:37 +0700

    Updates.

--- resources/views/sub_materi/footer_script.blade.php
+++ resources/views/sub_materi/footer_script.blade.php
@@ -10,12 +10,13 @@
         addEventListener("trix-attachment-add", function (event) {
             if (event.attachment.file) {
                 const file = event.attachment.file;
+                event.attachment.attachment.previewURL = URL.createObjectURL(file);
+
                 toBase64(file)
                     .then(base64 => {
                         event.attachment.setAttributes({
                             url: base64,
                         });
-
                         event.attachment.setUploadProgress(100)
                     })
                     .catch(error => {
