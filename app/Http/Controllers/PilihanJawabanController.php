7d95db0c49ac7aa51722b2d21a358c53e0a976eb
James Patrick Keegan <atomicbomber.email@gmail.com>
2020-02-17 13:36:02 +0700

    Updates.

--- app/Http/Controllers/PilihanJawabanController.php
+++ app/Http/Controllers/PilihanJawabanController.php
@@ -9,6 +9,13 @@ use Illuminate\Http\Request;
 
 class PilihanJawabanController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware([
+            "auth",
+        ]);
+    }
+
     /**
      * Display a listing of the resource.
      *
