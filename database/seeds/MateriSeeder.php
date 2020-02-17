cdf55162bbd4e107b7200dffe0f9618c785c86ed
James Patrick Keegan <atomicbomber.email@gmail.com>
2020-02-17 10:34:53 +0700

    Updates.

--- database/seeds/MateriSeeder.php
+++ database/seeds/MateriSeeder.php
@@ -26,11 +26,17 @@ class MateriSeeder extends Seeder
             $soals = factory(\App\Soal::class, 10)
                 ->create([
                     "materi_id" => $materi->id,
-                ]);
+                ])
+                ->each(function ($soal, $index) {
+                    $soal->update([ "konten" => "Soal " . ($index + 1) ]);
+                });
 
             foreach ($soals as $soal) {
                 $pilihan_jawabans = factory(\App\PilihanJawaban::class, 4)
-                    ->create([ "soal_id" => $soal->id ]);
+                    ->create([ "soal_id" => $soal->id ])
+                    ->each(function ($pilihan_jawaban, $index) {
+                        $pilihan_jawaban->update([ "konten" => "Pilihan Jawaban " . ($index + 1) ]);
+                    });
 
                 $soal->update([
                     "jawaban_benar_id" => $pilihan_jawabans->random()->id,
